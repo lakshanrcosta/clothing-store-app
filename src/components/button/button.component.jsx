ee5cb694ba0b5346be688c0ce22a4797479511cc
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-01 22:01:44 +0300

    Added button component

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,25 @@
+import './button.styles.scss';
+import PropTypes from 'prop-types';
+
+const BUTTON_TYPE_CLASSES = {
+  google: 'google-sigh-in',
+  inverted: 'inverted'
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} {...otherProps}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  buttonType: PropTypes.string,
+  otherProps: PropTypes.shape({
+    type: PropTypes.object.isRequired
+  })
+};
+
+export default Button;
