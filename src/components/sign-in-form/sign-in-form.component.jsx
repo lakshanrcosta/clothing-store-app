72d64cf333c094cde0f60968bafc34da60eee527
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-02 11:04:51 +0300

    Removed unused imports

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -3,7 +3,6 @@ import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 import {
   signInAuthUserWithEmailAndPassword,
-  createUserDocumentFromAuth,
   signInWithGooglePopup
 } from '../../utils/firebase/firebase.utils';
 import './sign-in-form.styles.scss';
