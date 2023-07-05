53ae4d5333bb2ba556175e0540e9ec9b9db6c63a
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-05 12:28:39 +0300

    Created card dropdown component

---
+++ src/components/cart-dropdown/card-dropdown.component.jsx
@@ -0,0 +1,11 @@
+import './card-dropdown.styles.scss';
+
+const CardDropdown = () => {
+  return (
+    <div className='cart-dropdown-container'>
+      <div className='cart-tem'></div>
+    </div>
+  );
+};
+
+export default CardDropdown;
