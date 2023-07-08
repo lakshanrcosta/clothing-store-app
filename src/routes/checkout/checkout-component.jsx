23b98f223b5f8994e74bab694e8dd55a052a0f25
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-08 23:01:45 +0300

    Added checkout headers and styles

--- src/routes/checkout/checkout-component.jsx
+++ src/routes/checkout/checkout-component.jsx
@@ -6,8 +6,25 @@ const Checkout = () => {
   const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
 
   return (
-    <div>
-      <h1>I am checkout page</h1>
+    <div className='checkout-container'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+
       {cartItems.map((cartItem) => {
         const { id, name, quantity } = cartItem;
         return (
@@ -19,6 +36,7 @@ const Checkout = () => {
           </div>
         );
       })}
+      <span>Total</span>
     </div>
   );
 };
