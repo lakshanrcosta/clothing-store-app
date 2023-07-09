4c48fbd8ccf1ba1b54e093d44b21a2af86299285
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-09 23:18:42 +0300

    Removed unused code comments

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,26 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
 import { getCollectionAndDocument } from '../utils/firebase/firebase.utils';
-// import PRODUCTS_DATA from '../data/products.data.json';
-
-// export const ProductsContext = createContext({
-//   products: [],
-//   setProducts: () => {}
-// });
-
-// export const ProductsProvider = ({ children }) => {
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     const getCategoryMap = async () => {
-//       const categoryMap = await getCollectionAndDocument();
-//       console.log(categoryMap);
-//     };
-//     getCategoryMap();
-//   }, []);
-
-//   const value = { products, setProducts };
-//   return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>;
-// };
 
 export const CategoriesContecxt = createContext({
   categoriesMap: {}
