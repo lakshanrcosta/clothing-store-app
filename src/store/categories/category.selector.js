46d979c9b6159afc92f8a16113a62e53c45f9022
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-16 13:22:05 +0300

    Removed return keyword and braces

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -2,9 +2,10 @@ import { createSelector } from 'reselect';
 
 const selectCategoryReducer = (state) => state.categories;
 
-export const selectCategories = createSelector([selectCategoryReducer], (categoriesSlice) => {
-  return categoriesSlice.categoriesArray;
-});
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categoriesArray
+);
 
 export const selectCategoriesMap = createSelector([selectCategories], (categories) =>
   categories.reduce((acc, category) => {
