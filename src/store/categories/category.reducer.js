d6e234ee4aebabf06f44552276f5d3af5793ecbf
Lakshan Costa <lakshanrcosta@gmail.com>
2023-07-18 11:17:03 +0300

    Refactor renamed categoriesArray to categories

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,14 +1,14 @@
 import { CATEGORIES_ACTION_TYPES } from './category.action-types';
 
 const INITIAL_CATEGORIES_STATE = {
-  categoriesArray: []
+  categories: []
 };
 
 export const categoriesReducer = (state = INITIAL_CATEGORIES_STATE, action) => {
   const { type, payload } = action;
   switch (type) {
     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categoriesArray: payload };
+      return { ...state, categories: payload };
     default:
       return state;
   }
